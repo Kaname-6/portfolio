b48cabaa337e0e9f35b857b2d1d8606d8ce45611
k24084kk <k24084kk@My-MacBook-Air.local>
2025-06-21 12:32:10 +0900

    WIP: 作業中の変更

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(express.json()); // JSONを扱う
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // データを受け取る
 app.post('/save/:type', (req, res) => {
   const type = req.params.type;
