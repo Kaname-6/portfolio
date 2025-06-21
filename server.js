561a2025cead2633dbe924f9fbf936b22ca16ddb
k24084kk <k24084kk@My-MacBook-Air.local>
2025-06-21 12:38:46 +0900

    home.htmlを

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
 // データを受け取る
